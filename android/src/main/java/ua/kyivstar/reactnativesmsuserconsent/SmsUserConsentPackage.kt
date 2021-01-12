a3e51675047248f74e8ddfea11fe41c827510b6a
Simonas Gildutis <simonasgildutis@gmail.com>
2021-01-12 16:30:11 +0200

    Update SmsUserConsentPackage.kt

--- android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsUserConsentPackage.kt
+++ android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsUserConsentPackage.kt
@@ -1,7 +1,5 @@
 package ua.kyivstar.reactnativesmsuserconsent
 
-import java.util.Arrays
-
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
@@ -9,7 +7,7 @@ import com.facebook.react.uimanager.ViewManager
 
 class SmsUserConsentPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return Arrays.asList<NativeModule>(SmsUserConsentModule(reactContext))
+        return listOf<NativeModule>(SmsUserConsentModule(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
