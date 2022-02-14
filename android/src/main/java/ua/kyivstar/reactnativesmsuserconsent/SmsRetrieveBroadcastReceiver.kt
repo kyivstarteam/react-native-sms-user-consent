e91e809de24fb1b7fd172fe3de0ecbf031e80a05
Yevhenii Onipko <y.onipko@kyivstar.net>
2022-02-14 17:20:14 +0200

    :construction: - refactor (#20)
    * :construction: - refactor
    
    * :construction: - refactor

--- android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsRetrieveBroadcastReceiver.kt
+++ android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsRetrieveBroadcastReceiver.kt
@@ -10,17 +10,21 @@ import com.google.android.gms.auth.api.phone.SmsRetriever;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.Status;
 
+
+
 class SmsRetrieveBroadcastReceiver(currentActivity: Activity?): BroadcastReceiver() {
+
+
   val SMS_CONSENT_REQUEST = 1244
 
   private var activity: Activity? = currentActivity
 
   override fun onReceive(context: Context?, intent: Intent) {
-    val extras = intent.extras
-    if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action && extras != null) {
-      val smsRetrieverStatus: Status = extras[SmsRetriever.EXTRA_STATUS] as Status
-      
-      when (smsRetrieverStatus.statusCode) {
+    if (SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.action)) {
+      val extras = intent.extras
+      val smsRetrieverStatus: Status = extras?.get(SmsRetriever.EXTRA_STATUS) as Status
+      val statusCode: Int = smsRetrieverStatus.getStatusCode()
+      when (statusCode) {
         CommonStatusCodes.SUCCESS ->                     // Get consent intent
           try {
             val consentIntent = extras.getParcelable<Intent>(SmsRetriever.EXTRA_CONSENT_INTENT)
